44f1a79d8803f8e3f4b2adac3a77746237081db1
Garcia Sébastien <34031275+Sebestala@users.noreply.github.com>
2024-09-19 18:58:04 +0200

    Installs and conf done

--- packages/ui/.eslintrc.js
+++ packages/ui/.eslintrc.js
@@ -1,3 +1,7 @@
 module.exports = {
-  extends: ["@repo/eslint-config/react.js"],
+  extends: [
+    "@repo/eslint-config/react.js",
+    "plugin:storybook/recommended",
+    "plugin:tailwindcss/recommended",
+  ],
 };
